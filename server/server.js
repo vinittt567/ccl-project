8e36aa5218eccb71052a42d73aefa94dbcc6a11a
vinittt567 <vinitkalambe12@gmail.com>
2026-04-17 00:01:14 +0530

    fix: remove invalid app.options wildcard pattern

--- server/server.js
+++ server/server.js
@@ -36,7 +36,6 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.options('*', cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static('public'));
